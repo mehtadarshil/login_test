522a85fa0828edcc856e1c6c17a9eef3fcd6ca9f
darshil.mehta <darshil.mehta>
2024-05-27 11:57:21 +0530

    dadsdsa

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const userController = require('./controller/user_controller.js');
 const app = express();
 var bodyParser = require('body-parser');
+const path = require('path');
 const port = 9000;
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-
+const filePath = path.join(__dirname, 'test.html');
 app.get('/', (req, res) => {
-  res.sendFile('test.html',{ root: '.' });
+  res.sendFile(filePath,(err) => {
+    if (err) {
+      console.error('Error sending file:', err);
+      // Send an error response if the file cannot be sent
+      return res.status(500).send('Internal Server Error');
+    }});
 });
 
 app.get('/getAllUsers', userController.getAllUser);
